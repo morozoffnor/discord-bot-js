9e263dcae295c19b15497ef3d29e7dd13f27d352
Igor Morozov <morozofffmia@gmail.com>
2019-12-26 03:40:50 +0300

    made things prettier

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -1,41 +1,35 @@
-import * as Discord from "discord.js";
-import { IBotCommand } from "../api";
+import * as Discord from 'discord.js';
+import { IBotCommand } from '../api';
 
 export default class kick implements IBotCommand {
-    
-    private readonly _command = "kick"
-    
-    help(): string {
-        return "Kicks the mentioned user";
-        // throw new Error("Method not implemented.");
-    }
-    isThisCommand(command: string): boolean {
-        return command === this._command;
-        // throw new Error("Method not implemented.");
-    }
-    runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): void {
-        
-        let mentionedUser = msgObject.mentions.users.first();
+	private readonly _command = 'kick';
 
-        let reason = args.slice(1).join(" ") || "";
-        let kicklog = `${msgObject.author.username}: ${reason}`;
+	help(): string {
+		return 'Kicks the mentioned user';
+		// throw new Error("Method not implemented.");
+	}
+	isThisCommand(command: string): boolean {
+		return command === this._command;
+		// throw new Error("Method not implemented.");
+	}
+	runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): void {
+		let mentionedUser = msgObject.mentions.users.first();
 
-        msgObject.delete(0);
-        
-        if(!msgObject.member.hasPermission("ADMINISTRATOR")) {
-            msgObject.channel.send(`Noice try, ${msgObject.author.username}, но у тебя нет прав на это дерьмо!`);
-            return;
-        }
+		let reason = args.slice(1).join(' ') || '';
+		let kicklog = `${msgObject.author.username}: ${reason}`;
 
-        if(!mentionedUser){
-            msgObject.channel.send(`Сорян, ${msgObject.author.username}, я не нашел такого пользователя.`);
-            return;
-        }
+		msgObject.delete(0);
 
+		if (!msgObject.member.hasPermission('ADMINISTRATOR')) {
+			msgObject.channel.send(`Noice try, ${msgObject.author.username}, но у тебя нет прав на это дерьмо!`);
+			return;
+		}
 
-        msgObject.guild.member(mentionedUser).kick(kicklog)
-            .then(console.log)
-            .catch(console.error)
-    }
+		if (!mentionedUser) {
+			msgObject.channel.send(`Сорян, ${msgObject.author.username}, я не нашел такого пользователя.`);
+			return;
+		}
 
+		msgObject.guild.member(mentionedUser).kick(kicklog).then(console.log).catch(console.error);
+	}
 }
