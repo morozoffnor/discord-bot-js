7aa76a54cf4b58cefe8e9a75b0525165a87c2c5b
Igor Morozov <morozofffmia@gmail.com>
2019-12-26 03:42:34 +0300

    yearprogress added
    paste your webhook link in config.js

--- src/index.ts
+++ src/index.ts
@@ -1,144 +1,344 @@
-import * as Discord from "discord.js";
-import * as Configfile from "./config";
-import { IBotCommand } from "./api";
-import * as fs from "fs";
+import * as Discord from 'discord.js';
+import * as Configfile from './config';
+import { IBotCommand } from './api';
+import * as fs from 'fs';
 
 const client: Discord.Client = new Discord.Client();
-
+const CronJob = require('cron').CronJob;
+const request = require('request');
 
 let commands: IBotCommand[] = [];
 
 loadCommands(`${__dirname}/commands`);
-client.on("ready", () => {
-    console.log(" ");
-    console.log("Using " + client.user.username + "#" + client.user.discriminator + " account!");
-    console.log(" ");
-    console.log("Connected servers and channels:");
-    client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name);
+client.on('ready', () => {
+	console.log(' ');
+	console.log('Using ' + client.user.username + '#' + client.user.discriminator + ' account!');
+	console.log(' ');
+	console.log('Connected servers and channels:');
+	client.guilds.forEach((guild) => {
+		console.log(' - ' + guild.name);
 
-        guild.channels.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
-        });
-    });
-    console.log(" ");
-    console.log("Setting status");
-    changeStatus(client);
+		guild.channels.forEach((channel) => {
+			console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
+		});
+	});
+	console.log(' ');
+	console.log('Setting status');
+	changeStatus(client);
 
-     
-    var hourMillseconds = 1000 * 60 *60;
-    setInterval(function(){ 
-        changeStatus(client)
-    }, hourMillseconds)
-    
-    
-    console.log("ready to go!");
-    
-    
-})
+	var hourMillseconds = 1000 * 60 * 60;
+	setInterval(function() {
+		changeStatus(client);
+	}, hourMillseconds);
 
-client.on("message", msg => {
+	console.log('ready to go!');
+});
 
-    // игнорировать сообщения ботов
-    if (msg.author.bot) { return; }
+client.on('message', (msg) => {
+	// игнорировать сообщения ботов
+	if (msg.author.bot) {
+		return;
+	}
 
-    // игнорировать сообщения без префикса
-    if (!msg.content.startsWith(Configfile.config.prefix)) { return };
-    
-    // обработать команду
-    handeCommand(msg);
-})
+	// игнорировать сообщения без префикса
+	if (!msg.content.startsWith(Configfile.config.prefix)) {
+		return;
+	}
+
+	// обработать команду
+	handeCommand(msg);
+});
 
 async function handeCommand(msg: Discord.Message) {
-    // split the sstring into the command and args
-    let command = msg.content.split(" ")[0].replace(Configfile.config.prefix, "");
-    let args = msg.content.split(" ").slice(1);
+	// split the sstring into the command and args
+	let command = msg.content.split(' ')[0].replace(Configfile.config.prefix, '');
+	let args = msg.content.split(' ').slice(1);
 
-    // loop through all of loaded commands
-    for (const commandClass of commands) {
-        // try to execute and ready in case of an error
-        try {
-            // check command class if it is a correct one
-            if (!commandClass.isThisCommand(command)) {
-                // go to the next iteration if this isn't the correct one
-                continue;
-            }
-            // 
-            await commandClass.runCommand(args, msg, client);
-        }
-        catch (exeption) {
-            console.log(exeption);
-        }
-    }
+	// loop through all of loaded commands
+	for (const commandClass of commands) {
+		// try to execute and ready in case of an error
+		try {
+			// check command class if it is a correct one
+			if (!commandClass.isThisCommand(command)) {
+				// go to the next iteration if this isn't the correct one
+				continue;
+			}
+			//
+			await commandClass.runCommand(args, msg, client);
+		} catch (exeption) {
+			console.log(exeption);
+		}
+	}
 }
 
 function loadCommands(commandsPath: string) {
-    //если нет команд - оффнуться
-    if (!Configfile.config || (Configfile.config.commands as string[]).length === 0) {
-        return;
-    }
-    //проверить все команды
-    for (const commandName of Configfile.config.commands as string[]) {
-        const commandsClass = require(`${commandsPath}/${commandName}`).default;
+	//если нет команд - оффнуться
+	if (!Configfile.config || (Configfile.config.commands as string[]).length === 0) {
+		return;
+	}
+	//проверить все команды
+	for (const commandName of Configfile.config.commands as string[]) {
+		const commandsClass = require(`${commandsPath}/${commandName}`).default;
 
-        const command = new commandsClass() as IBotCommand;
+		const command = new commandsClass() as IBotCommand;
 
-        commands.push(command);
-    }
+		commands.push(command);
+	}
 }
-
+// TODO вот это сделать нормально
 function changeStatus(client: Discord.Client) {
-    let statusString = "";
-    let statusURL = "";
-    
-    fs.readFile('./content/statusString.txt', function (err, data) {
-        if (err)
-            throw err;
-        let statusArr = data.toString().split("\n");
-        let random = Math.floor((Math.random() * statusArr.length));
-        statusString = statusArr[random];
-        
-        let done1 = true;
-        if (done1) {
-            fs.readFile('./content/statusURL.txt', function (err, data) {
-                if (err)
-                    throw err;
-                let statusURLArr = data.toString().split("\n");
-                let random = Math.floor((Math.random() * statusURLArr.length));
-                statusURL = statusURLArr[random];
-                
-                let done2 = true;
-                if (done2) {
-                    let statusId = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
-                    let statuses = ["PLAYING", "STREAMING", "LISTENING", "WATCHING"];
-                    if (statusId == 1) {
-                        client.user.setActivity(statusString, {
-                            type: "STREAMING",
-                            url: statusURL
-                        });
-                        
-                    } else {
-                        let done3 = true;
-                    if (done3) {
-                        client.user.setActivity(statusString, {
-                            type: statusId,
-                            url: statusURL
-                        });
-                        // need to fix this
-                        console.log(`Status has changed to ${statuses[statusId]}, ${statusString} with url: ${statusURL}`);
-                    }
-                    
-                    }
-                }
-            });
-        }
-    });
+	let statusString = '';
+	let statusURL = '';
+
+	fs.readFile('./content/statusString.txt', function(err, data) {
+		if (err) throw err;
+		let statusArr = data.toString().split('\n');
+		let random = Math.floor(Math.random() * statusArr.length);
+		statusString = statusArr[random];
+
+		let done1 = true;
+		if (done1) {
+			fs.readFile('./content/statusURL.txt', function(err, data) {
+				if (err) throw err;
+				let statusURLArr = data.toString().split('\n');
+				let random = Math.floor(Math.random() * statusURLArr.length);
+				statusURL = statusURLArr[random];
+
+				let done2 = true;
+				if (done2) {
+					let statusId = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+					let statuses = [ 'PLAYING', 'STREAMING', 'LISTENING', 'WATCHING' ];
+					if (statusId == 1) {
+						client.user.setActivity(statusString, {
+							type: 'STREAMING',
+							url: statusURL
+						});
+					} else {
+						let done3 = true;
+						if (done3) {
+							client.user.setActivity(statusString, {
+								type: statusId,
+								url: statusURL
+							});
+							// need to fix this
+							console.log(
+								`Status has changed to ${statuses[statusId]}, ${statusString} with url: ${statusURL}`
+							);
+						}
+					}
+				}
+			});
+		}
+	});
 }
 
+// ========== Year progress daily post =============
 
+// Define year, and the current year etc.. math..
+const days = 365;
 
-/*
- 
-*/
+// Calc one day
+const oneDay = 1000 * 60 * 60 * 24;
+
+let now = new Date();
+let start = new Date(now.getFullYear(), 0, 0);
+let diff = Math.abs(<any>new Date() - <any>new Date(start));
+let currentDay = Math.floor(diff / oneDay);
+let currentPerc = currentDay / days * 100;
+
+// Unicode Bar Styles
+const bar_styles = [
+	'▁▂▃▄▅▆▇█',
+	'⣀⣄⣤⣦⣶⣷⣿',
+	'⣀⣄⣆⣇⣧⣷⣿',
+	'○◔◐◕⬤',
+	'□◱◧▣■',
+	'□◱▨▩■',
+	'□◱▥▦■',
+	'░▒▓█',
+	'░█',
+	'⬜⬛',
+	'▱▰',
+	'▭◼',
+	'▯▮',
+	'◯⬤',
+	'⚪⚫'
+];
+
+const emojis = [
+	'🕐',
+	'🕑',
+	'🕒',
+	'🕓',
+	'🕔',
+	'🕕',
+	'🕖',
+	'🕗',
+	'🕘',
+	'🕙',
+	'🕚',
+	'🕛',
+	'🕜',
+	'🕝',
+	'🕞',
+	'🕟',
+	'🕠',
+	'🕡',
+	'🕢',
+	'🕣',
+	'🕤',
+	'🕥',
+	'🕦',
+	'🕧',
+	'🐶',
+	'🐱',
+	'🐭',
+	'🐹',
+	'🐰',
+	'🦊',
+	'🦝',
+	'🐻',
+	'🐼',
+	'🦠',
+	'🐢',
+	'🐍',
+	'🦎',
+	'🦖',
+	'🦕',
+	'🐙',
+	'🦑',
+	'🦐',
+	'🦀',
+	'🐡',
+	'🐠',
+	'🐟',
+	'🐬',
+	'🐳',
+	'🐋',
+	'🦈',
+	'🐊',
+	'🐅',
+	'🐆',
+	'🦓',
+	'🦍',
+	'🐘',
+	'🦏',
+	'🦛',
+	'🐪',
+	'🐫',
+	'🦙',
+	'🦒',
+	'🐃',
+	'🐂',
+	'🐄',
+	'🐎',
+	'🐖',
+	'🐏',
+	'🐑',
+	'🐐',
+	'🦌',
+	'🐕',
+	'🐩',
+	'🐈',
+	'🐓',
+	'🦃',
+	'🕊',
+	'🐇',
+	'🐁',
+	'🐀',
+	'🐿',
+	'🦔'
+];
+
+// Show them in the console
+console.log('Current day : ' + currentDay);
+console.log('Current perc : ' + currentPerc);
+console.log('Testing the bar' + make_bar(currentPerc, bar_styles[0]) + ' ' + currentPerc.toFixed(2) + '%');
+
+// Cronjob for daily post
+new CronJob(
+	'0 0 * * *',
+	function() {
+		YearProgressEveryDay();
+		postyearProgress(client, currentPerc, bar_styles);
+	},
+	null,
+	true,
+	'UTC'
+);
+
+function YearProgressEveryDay() {
+	now = new Date();
+	start = new Date(now.getFullYear(), 0, 0);
+	diff = Math.abs(<any>new Date() - <any>new Date(start));
+	currentDay = Math.floor(diff / oneDay);
+	currentPerc = currentDay / days * 100;
+}
+
+function postyearProgress(client: Discord.Client, currentPerc: number, bar_styles: any[]) {
+	let randomemojiId = Math.floor(Math.random() * (emojis.length - 0)) + +0;
+	let emojismile = emojis[randomemojiId];
+
+	var bar = make_bar(currentPerc, bar_styles[0]) + '   ' + currentPerc.toFixed(2) + '% #YearProgress ' + emojismile;
+
+	for (var i = 0; i <= Configfile.config.yearporgresswebhooks.length; i++) {
+		request.post(
+			Configfile.config.yearporgresswebhooks[i],
+			{
+				json: {
+					content: bar
+				}
+			},
+			(error: any, res: { statusCode: any }, body: any) => {
+				if (error) {
+					console.error(error);
+					return;
+				}
+				console.log(`statusCode: ${res.statusCode}`);
+				console.log(body);
+			}
+		);
+	}
+}
+
+function repeat(s: string, i: number) {
+	var r = '';
+	for (var j = 0; j < i; j++) r += s;
+	return r;
+}
+
+function make_bar(perc: number, bar_style: string | any[]) {
+	var p = perc;
+
+	var d,
+		full,
+		m,
+		middle,
+		r,
+		rest,
+		x,
+		min_delta = Number.POSITIVE_INFINITY,
+		full_symbol = bar_style[bar_style.length - 1],
+		n = bar_style.length - 1;
+	if (p == 100) return repeat(full_symbol, 10);
+	p = p / 100;
+
+	for (var i = 25; i >= 1; i--) {
+		x = p * i;
+		full = Math.floor(x);
+		rest = x - full;
+		middle = Math.floor(rest * n);
+		if (p != 0 && full == 0 && middle == 0) middle = 1;
+		d = Math.abs(p - (full + middle / n) / i) * 100;
+		if (d < min_delta) {
+			min_delta = d;
+			m = bar_style[middle];
+			if (full == i) m = '';
+			r = repeat(full_symbol, full) + m + repeat(bar_style[0], i - full - 1);
+		}
+
+		return r;
+	}
+}
 
 client.login(Configfile.config.token);
