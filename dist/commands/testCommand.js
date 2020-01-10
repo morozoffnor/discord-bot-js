ab64fe0426e732f3369d9844cc6ed14d7ca79cf3
Igor Morozov <morozofffmia@gmail.com>
2020-01-10 23:42:59 +0300

    changed nothing
    literally

--- dist/commands/testCommand.js
+++ dist/commands/testCommand.js
@@ -2,17 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class testCommand {
     constructor() {
-        this._command = "testCommand";
+        this._command = 'testCommand';
     }
     help() {
-        return "This is nothing";
+        return 'This is nothing';
     }
     isThisCommand(command) {
         return command === this._command;
     }
     runCommand(args, msgObject, client) {
-        msgObject.channel.send("Обработчик команд заработал и я крутой");
+        msgObject.channel.send('Обработчик команд заработал и я крутой');
     }
 }
 exports.default = testCommand;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdENvbW1hbmQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY29tbWFuZHMvdGVzdENvbW1hbmQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFHQSxNQUFxQixXQUFXO0lBQWhDO1FBRXFCLGFBQVEsR0FBRyxhQUFhLENBQUE7SUFlN0MsQ0FBQztJQWJHLElBQUk7UUFDQSxPQUFPLGlCQUFpQixDQUFDO0lBRTdCLENBQUM7SUFDRCxhQUFhLENBQUMsT0FBZTtRQUN6QixPQUFPLE9BQU8sS0FBSyxJQUFJLENBQUMsUUFBUSxDQUFDO0lBRXJDLENBQUM7SUFDRCxVQUFVLENBQUMsSUFBYyxFQUFFLFNBQTBCLEVBQUUsTUFBc0I7UUFDekUsU0FBUyxDQUFDLE9BQU8sQ0FBQyxJQUFJLENBQUMsd0NBQXdDLENBQUMsQ0FBQztJQUVyRSxDQUFDO0NBRUo7QUFqQkQsOEJBaUJDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdENvbW1hbmQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY29tbWFuZHMvdGVzdENvbW1hbmQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFHQSxNQUFxQixXQUFXO0lBQWhDO1FBQ2tCLGFBQVEsR0FBRyxhQUFhLENBQUM7SUFjM0MsQ0FBQztJQVpBLElBQUk7UUFDSCxPQUFPLGlCQUFpQixDQUFDO0lBRTFCLENBQUM7SUFDRCxhQUFhLENBQUMsT0FBZTtRQUM1QixPQUFPLE9BQU8sS0FBSyxJQUFJLENBQUMsUUFBUSxDQUFDO0lBRWxDLENBQUM7SUFDRCxVQUFVLENBQUMsSUFBYyxFQUFFLFNBQTBCLEVBQUUsTUFBc0I7UUFDNUUsU0FBUyxDQUFDLE9BQU8sQ0FBQyxJQUFJLENBQUMsd0NBQXdDLENBQUMsQ0FBQztJQUVsRSxDQUFDO0NBQ0Q7QUFmRCw4QkFlQyJ9
--- src/commands/testCommand.ts
+++ src/commands/testCommand.ts
@@ -1,21 +1,19 @@
-import * as Discord from "discord.js";
-import { IBotCommand } from "../api";
+import * as Discord from 'discord.js';
+import { IBotCommand } from '../api';
 
 export default class testCommand implements IBotCommand {
-    
-    private readonly _command = "testCommand"
-    
-    help(): string {
-        return "This is nothing";
-        // throw new Error("Method not implemented.");
-    }
-    isThisCommand(command: string): boolean {
-        return command === this._command;
-        // throw new Error("Method not implemented.");
-    }
-    runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): void {
-        msgObject.channel.send("Обработчик команд заработал и я крутой");
-        // throw new Error("Method not implemented.");
-    }
+	private readonly _command = 'testCommand';
 
+	help(): string {
+		return 'This is nothing';
+		// throw new Error("Method not implemented.");
+	}
+	isThisCommand(command: string): boolean {
+		return command === this._command;
+		// throw new Error("Method not implemented.");
+	}
+	runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): void {
+		msgObject.channel.send('Обработчик команд заработал и я крутой');
+		// throw new Error("Method not implemented.");
+	}
 }
