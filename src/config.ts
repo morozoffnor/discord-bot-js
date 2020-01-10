0cf9b6c991b2ef1fd5e63364ce924e39a066877a
Igor Morozov <morozofffmia@gmail.com>
2020-01-10 23:43:29 +0300

    commands update
    new commands were added

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export let config = {
 	token: 'your_token_here',
 	prefix: '!',
-	commands: [ 'dota', 'ti', 'roll', 'yearprogress' ],
+	commands: [ 'dota', 'ti', 'roll', 'yearprogress', 'emoji', 'lock', 'unlock', 'story' ],
 	restricted: [ '/midi/' ],
 	yearporgresswebhooks: [ 'webhook_link_1', 'webhook_link_2' ]
 };
