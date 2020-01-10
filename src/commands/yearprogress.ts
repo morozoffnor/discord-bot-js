2e44436682c4e7ab02c294d09c14b15ab39ba10f
Igor Morozov <morozofffmia@gmail.com>
2020-01-10 23:44:45 +0300

    manual yearprogress
    displays current percent.
    will change emojis some day

--- src/commands/yearprogress.ts
+++ src/commands/yearprogress.ts
@@ -1,111 +1,200 @@
-import * as Discord from "discord.js";
-import { IBotCommand } from "../api";
-import * as fs from "fs";
-import * as Configfile from "../config";
+import * as Discord from 'discord.js';
+import { IBotCommand } from '../api';
+import * as fs from 'fs';
+import * as Configfile from '../config';
 const request = require('request');
 
 export default class yearprogress implements IBotCommand {
-    
-    private readonly _command = "yearprogress"
-    
-    help(): string {
-        return "This is nothing";
-        // throw new Error("Method not implemented.");
-    }
-    isThisCommand(command: string): boolean {
-        return command === this._command;
-        // throw new Error("Method not implemented.");
-    }
-    runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): void {
+	private readonly _command = 'yearprogress';
 
-        // check if this command is allowed on this server
-        for (let key of Configfile.config.restricted) {
-            if (msgObject.guild.name == key) {
-                return;
-            }
-        }
+	help(): string {
+		return 'This is nothing';
+		// throw new Error("Method not implemented.");
+	}
+	isThisCommand(command: string): boolean {
+		return command === this._command;
+		// throw new Error("Method not implemented.");
+	}
+	runCommand(args: string[], msgObject: Discord.Message, client: Discord.Client): void {
+		// check if this command is allowed on this server
+		for (let key of Configfile.config.restricted) {
+			if (msgObject.guild.name == key) {
+				return;
+			}
+		}
 
-        const emojis = ['🕐', '🕑', '🕒', '🕓', '🕔', '🕕', '🕖', '🕗', '🕘', '🕙',
-            '🕚', '🕛', '🕜', '🕝', '🕞', '🕟', '🕠', '🕡', '🕢', '🕣', '🕤', '🕥',
-            '🕦', '🕧', '🐶', '🐱', '🐭', '🐹', '🐰', '🦊', '🦝', '🐻', '🐼', '🦠',
-            '🐢', '🐍', '🦎', '🦖', '🦕', '🐙', '🦑', '🦐', '🦀', '🐡', '🐠', '🐟',
-            '🐬', '🐳', '🐋', '🦈', '🐊', '🐅', '🐆', '🦓', '🦍', '🐘', '🦏', '🦛',
-            '🐪', '🐫', '🦙', '🦒', '🐃', '🐂', '🐄', '🐎', '🐖', '🐏', '🐑', '🐐',
-            '🦌', '🐕', '🐩', '🐈', '🐓', '🦃', '🕊', '🐇', '🐁', '🐀', '🐿', '🦔']
+		let days = 366;
+		let oneDay = 1000 * 60 * 60 * 24;
 
-        const bar_styles = [
-        '▁▂▃▄▅▆▇█',
-        '⣀⣄⣤⣦⣶⣷⣿',
-        '⣀⣄⣆⣇⣧⣷⣿',
-        '○◔◐◕⬤',
-        '□◱◧▣■',
-        '□◱▨▩■',
-        '□◱▥▦■',
-        '░▒▓█',
-        '░█',
-        '⬜⬛',
-        '▱▰',
-        '▭◼',
-        '▯▮',
-        '◯⬤',
-        '⚪⚫',
-        ];
+		let now = new Date();
+		let start = new Date(now.getFullYear(), 0, 0);
+		let diff = Math.abs(<any>new Date() - <any>new Date(start));
+		let currentDay = Math.floor(diff / oneDay);
+		let currentPerc = currentDay / days * 100;
 
-        // execute
-        let days = 365;
-        let oneDay = 1000 * 60 * 60 * 24;
+		let embedColor = Math.ceil(Math.floor(Math.random() * (16777213 - 0 + 1)) + 0);
 
-        let now = new Date();
-        let start = new Date(now.getFullYear(), 0, 0);
-        let diff = Math.abs(<any>new Date() - <any>new Date(start));
-        let currentDay = Math.floor(diff / oneDay);
-        let currentPerc = currentDay / days * 100
+		const emojis = [
+			'🕐',
+			'🕑',
+			'🕒',
+			'🕓',
+			'🕔',
+			'🕕',
+			'🕖',
+			'🕗',
+			'🕘',
+			'🕙',
+			'🕚',
+			'🕛',
+			'🕜',
+			'🕝',
+			'🕞',
+			'🕟',
+			'🕠',
+			'🕡',
+			'🕢',
+			'🕣',
+			'🕤',
+			'🕥',
+			'🕦',
+			'🕧',
+			'🐶',
+			'🐱',
+			'🐭',
+			'🐹',
+			'🐰',
+			'🦊',
+			'🦝',
+			'🐻',
+			'🐼',
+			'🦠',
+			'🐢',
+			'🐍',
+			'🦎',
+			'🦖',
+			'🦕',
+			'🐙',
+			'🦑',
+			'🦐',
+			'🦀',
+			'🐡',
+			'🐠',
+			'🐟',
+			'🐬',
+			'🐳',
+			'🐋',
+			'🦈',
+			'🐊',
+			'🐅',
+			'🐆',
+			'🦓',
+			'🦍',
+			'🐘',
+			'🦏',
+			'🦛',
+			'🐪',
+			'🐫',
+			'🦙',
+			'🦒',
+			'🐃',
+			'🐂',
+			'🐄',
+			'🐎',
+			'🐖',
+			'🐏',
+			'🐑',
+			'🐐',
+			'🦌',
+			'🐕',
+			'🐩',
+			'🐈',
+			'🐓',
+			'🦃',
+			'🕊',
+			'🐇',
+			'🐁',
+			'🐀',
+			'🐿',
+			'🦔'
+		];
 
-        let randomemojiId = Math.floor(Math.random() * (emojis.length - 0)) + +0;
-        let emojismile = emojis[randomemojiId];
+		const bar_styles = [
+			'▁▂▃▄▅▆▇█',
+			'⣀⣄⣤⣦⣶⣷⣿',
+			'⣀⣄⣆⣇⣧⣷⣿',
+			'○◔◐◕⬤',
+			'□◱◧▣■',
+			'□◱▨▩■',
+			'□◱▥▦■',
+			'░▒▓█',
+			'░█',
+			'⬜⬛',
+			'▱▰',
+			'▭◼',
+			'▯▮',
+			'◯⬤',
+			'⚪⚫'
+		];
 
-    
+		// execute
 
-        var bar = make_bar(currentPerc, bar_styles[0]) + '   ' + currentPerc.toFixed(2) + '% #YearProgress ' + emojismile;
+		let randomemojiId = Math.floor(Math.random() * (emojis.length - 0)) + +0;
+		let emojismile = emojis[randomemojiId];
 
-        msgObject.channel.send(bar);
+		let r = Math.ceil(Math.floor(Math.random() * (254 - 0 + 1)) + 0);
+		let g = Math.ceil(Math.floor(Math.random() * (254 - 0 + 1)) + 0);
+		let b = Math.ceil(Math.floor(Math.random() * (254 - 0 + 1)) + 0);
+		console.log(`embed color generated ${r} ${g} ${b}`);
 
-        function repeat(s: string, i: number) {
-            var r = '';
-            for (var j = 0; j < i; j++) r += s;
-            return r;
-        }
+		var bar = make_bar(currentPerc, bar_styles[2]);
+		let yearprogressEmbed = new Discord.RichEmbed()
+			.setTitle(`${currentPerc.toFixed(3)}% #YearProgress`)
+			.setColor([ r, g, b ])
+			.setDescription(`${bar}`)
+			.setFooter(`${emojismile}`)
+			.setTimestamp();
 
-        function make_bar(perc: number, bar_style: string | any[]) {
+		msgObject.channel.send(yearprogressEmbed);
 
-            var p = perc;
-        
-            var d, full, m, middle, r, rest, x,
-                min_delta = Number.POSITIVE_INFINITY,
-                full_symbol = bar_style[bar_style.length - 1],
-                n = bar_style.length - 1;
-            if (p == 100)
-                return repeat(full_symbol, 10);
-            p = p / 100;
-        
-            for (var i = 25; i >= 1; i--) {
-                x = p * i;
-                full = Math.floor(x);
-                rest = x - full;
-                middle = Math.floor(rest * n);
-                if (p != 0 && full == 0 && middle == 0) middle = 1;
-                d = Math.abs(p - (full + middle / n) / i) * 100;
-                if (d < min_delta) {
-                    min_delta = d;
-                    m = bar_style[middle];
-                    if (full == i) m = '';
-                    r = repeat(full_symbol, full) + m + repeat(bar_style[0], i - full - 1);
-                }
-        
-                return r;
-            }
-        
-        }
-    }
+		function repeat(s: string, i: number) {
+			var r = '';
+			for (var j = 0; j < i; j++) r += s;
+			return r;
+		}
 
+		function make_bar(perc: number, bar_style: string | any[]) {
+			var p = perc;
+
+			var d,
+				full,
+				m,
+				middle,
+				r,
+				rest,
+				x,
+				min_delta = Number.POSITIVE_INFINITY,
+				full_symbol = bar_style[bar_style.length - 1],
+				n = bar_style.length - 1;
+			if (p == 100) return repeat(full_symbol, 10);
+			p = p / 100;
+
+			for (var i = 25; i >= 1; i--) {
+				x = p * i;
+				full = Math.floor(x);
+				rest = x - full;
+				middle = Math.floor(rest * n);
+				if (p != 0 && full == 0 && middle == 0) middle = 1;
+				d = Math.abs(p - (full + middle / n) / i) * 100;
+				if (d < min_delta) {
+					min_delta = d;
+					m = bar_style[middle];
+					if (full == i) m = '';
+					r = repeat(full_symbol, full) + m + repeat(bar_style[0], i - full - 1);
+				}
+
+				return r;
+			}
+		}
+	}
 }
